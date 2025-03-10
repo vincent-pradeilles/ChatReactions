c5260b5d07eb7dc7007b5ef5e275ec2cdfe1229a
Vincent <>
2025-03-10 16:45:48 +0100

    Initial commit

---
+++ ChatReaction/ChatReactionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatReactionApp.swift
+//  ChatReaction
+//
+//  Created by Vincent on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatReactionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
